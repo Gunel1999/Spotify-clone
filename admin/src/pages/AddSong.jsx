c85a8c07bff929c07c91b3f0f8e305e5407eb265
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-07-24 18:11:52 +0400

    Admin updates

--- admin/src/pages/AddSong.jsx
+++ admin/src/pages/AddSong.jsx
@@ -1,8 +1,58 @@
 import React from 'react';
+import { assets } from '../assets/admin-assets/assets';
 
 const AddSong = () => {
   return (
-    <form className="flex flex-col items-start gap-8 text-gray-600"></form>
+    <form className="flex flex-col items-start gap-8 text-gray-600">
+      <div className="flex gap-8">
+        <div className="flex flex-col gap-4">
+          <p>Upload song</p>
+          <input type="file" id="song" accept="audio/*" hidden />
+          <label htmlFor="song">
+            <img
+              className="w-24 cursor-pointer"
+              src={assets.upload_song}
+              alt=""
+            />
+          </label>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p>Upload Image</p>
+          <input type="file" id="image" accept="image/*" hidden />
+          <label htmlFor="image">
+            <img
+              className="w-24 cursor-pointer"
+              src={assets.upload_area}
+              alt=""
+            />
+          </label>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2.5">
+        <p>Song Name</p>
+        <input
+          className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]"
+          type="text"
+          placeholder="Type Here"
+          required
+        />
+      </div>
+      <div className="flex flex-col gap-2.5">
+        <p>Song Description</p>
+        <input
+          className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]"
+          type="text"
+          placeholder="Type Here"
+          required
+        />
+      </div>
+      <div className="flex flex-col gap-2.5">
+        <p>Album</p>
+        <select className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[150px]">
+          <option value="none">None</option>
+        </select>
+      </div>
+    </form>
   );
 };
 
